78c1ba29c9e0d40ec456c4e9648fb1561b7c386e
Chris <chris.wilde1978@gmail.com>
2017-10-12 07:34:49 +0100

    Update Gui_Box.py

--- Gui_Box.py
+++ Gui_Box.py
@@ -1,6 +1,9 @@
 from guizero import App, Box, Text
+
 app = App(title="My app", height=300, width=200)
 box = Box(app)
-text1 = Text(box, text="Hello from the box", size=14, color="red", font="Arial")
-text2 = Text(app, text="Hello from the app", size=14, color="blue", font="Courier New")
+
+boxText = Text(box, text="Hello from the box", size=14, color="red", font="Arial")
+appText = Text(app, text="Hello from the app", size=14, color="blue", font="Courier New")
+
 app.display()
